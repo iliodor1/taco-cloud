f3cfcbd207bd11bc7a394ec8e0ee714f3e2f7f11
Eldar <eldar.gai@yandex.ru>
2022-06-07 22:52:23 +0500

    feat: add 'get' test home page method

---
+++ src/test/java/ru/ufa/eldar/tacocloud/controller/HomeControllerTest.java
@@ -0,0 +1,25 @@
+package ru.ufa.eldar.tacocloud.controller;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+public class HomeControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Welcome to...")));
+    }
+}
